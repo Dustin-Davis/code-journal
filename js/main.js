1f6cb632ec2663a683eab0ff333eb435f136c4e3
Dustin Davis <drpookydavis@comcast.net>
2021-12-21 13:09:32 -0800

    user-can-create-an-array

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ var $image = document.querySelector('.img');
 $photo.addEventListener('input', function (event) {
   $image.setAttribute('src', event.target.value);
 });
+
+var $form = document.querySelector('.form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entry = {
+    entryId: data.nextEntryId,
+    title: $form.elements.Title.value,
+    photoUrl: $form.elements.photoUrl.value,
+    notes: $form.elements.Notes.value
+  };
+  data.nextEntryId++;
+
+  data.entries.unshift(entry);
+  localStorage.setItem('data-profile', JSON.stringify(data));
+  $image.setAttribute('src', './images/placeholder-image-square.jpg');
+  $form.reset();
+});
