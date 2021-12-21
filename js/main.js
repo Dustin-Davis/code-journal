a4ba02f88f581b3c8fe3453c6d69188566e2c541
Dustin Davis <drpookydavis@comcast.net>
2021-12-20 17:36:20 -0800

    Listen for 'input' events on the photoUrl input to update the src attribute of the photo preview when the input value changes. #3

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 /* global data */
 /* exported data */
+
+var $photo = document.querySelector('.photoUrl');
+var $image = document.querySelector('.img');
+
+$photo.addEventListener('input', function (event) {
+  $image.setAttribute('src', event.target.value);
+});
